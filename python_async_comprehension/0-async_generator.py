9ad33c50393bdaf4c92721c9c9969b1ac371f2ca
Antgst <ant.gousset@gmail.com>
2026-04-08 10:16:14 +0200

    fix: fix T0 for checker, change the docstring and add a newline

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""Module that provides an asynchronous generator of random numbers."""
+"""Module for asynchronous generator."""
 
-import random
 import asyncio
+import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield 10 random floats between 0 and 10 with a 1-second delay."""
+    """Yield 10 random floating-point numbers after waiting one second each time."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
