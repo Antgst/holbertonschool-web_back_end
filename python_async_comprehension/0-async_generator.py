24dde1b45480de02c9851cbcc019df1b10c0517f
Antgst <ant.gousset@gmail.com>
2026-04-08 10:17:36 +0200

    fix: reduce the docstring line too long

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,8 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield 10 random floating-point numbers after waiting one second each time."""
+    """Yield 10 random floating-point numbers after
+    waiting one second each time."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
