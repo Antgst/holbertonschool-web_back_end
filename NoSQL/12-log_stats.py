1c2d874a4c58a00e490e9927bb98b55d720de588
Antgst <ant.gousset@gmail.com>
2026-04-22 16:36:41 +0200

    feat: fix T10 and add T12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
-"""Module to update topics of a school"""
+"""Provides stats about Nginx logs stored in MongoDB."""
+
+from pymongo import MongoClient
 
 
-def update_topics(mongo_collection, name, topics):
-    """Update all topics of a school document based on the name"""
-    mongo_collection.update_many(
-        {"name": name},
-        {"$set": {"topics": topics}}
-    )
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    status_count = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+    print("{} status check".format(status_count))
