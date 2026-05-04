a7a88d3a636f9de3592d38d2187086b0bcc5e395
Antgst <ant.gousset@gmail.com>
2026-05-04 09:44:26 +0200

    add: install the depedencies of the project

---
+++ ES6_data_manipulation/babel.config.js
@@ -0,0 +1,14 @@
+cat > babel.config.js <<'EOF'
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
+EOF
